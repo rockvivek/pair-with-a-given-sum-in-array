c0fed4ad2de0911b2d59e933c9b52e302dae67d8
vivek shrivastwa <61682978+rockvivek@users.noreply.github.com>
2020-03-17 02:03:57 +0530

    find pair using hash map in O(n) time complexity
    updated

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
 #include <stdio.h>
+#include<bits/stdc++.h>
+//function to find a pair in an array with given sum using the hashing technique....
+using namespace std;
 
-// Naive method to find a pair in an array with given sum
 void findPair(int arr[], int n, int sum)
 {
-	// consider each element except last element
-	for (int i = 0; i < n - 1; i++)
-	{
-		// start from i'th element till last element
-		for (int j = i + 1; j < n; j++)
-		{
-			// if desired sum is found, print it and return
-			if (arr[i] + arr[j] == sum)
-			{
-				printf("Pair found at index %d and %d", i, j);
-				return;
-			}
-		}
-	}
+    unordered_map<int,int> um;
 
-	// No pair with given sum exists in the array
-	printf("Pair not found");
+    for(int i=0;i<n;i++){
+        if(um.find(sum-arr[i])!=um.end()){
+            cout<<"pair found at index "<<um[sum-arr[i]]<<" and " <<i<<endl;
+            return ;
+        }
+        else{
+            um[arr[i]] = i;
+        }
+    }
+    cout<<"pair not found"<<endl;
 }
 
 // main function
